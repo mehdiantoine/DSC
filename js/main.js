96c0e328d23a5749d39d47cf8165e73f8500f2d6
mehdiantoine <mehdiantoinechikh@gmail.com>
2019-05-27 23:33:50 +0200

    responsive&stuff

--- js/main.js
+++ js/main.js
@@ -1,28 +1,31 @@
-<<<<<<< HEAD
 "use strict";
 
 
 var init = function (){
 
+    var menu, nav;
+    menu = $('.title-header');
+    nav = $('.nav-header');
+
+    if(window.matchMedia("(max-width: 750px)").matches) {
+        /* La largeur minimum de l'affichage est 600 px inclus */
+        nav.add('.hidden');
+
+    }
+
+    var onClickMenu = function () {
+
+        nav.slideToggle();
+
+    };
+
+    menu.click(onClickMenu);
+
 };
 
 
-$(function (){
+$(function () {
 
     init();
 
-=======
-"use strict";
-
-
-var init = function (){
-
-};
-
-
-$(function (){
-
-    init();
-
->>>>>>> origin/master
 });
