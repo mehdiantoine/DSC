b3469356520dd65100b7e591373d471d6e1fe16f
mehdiantoine <mehdiantoinechikh@gmail.com>
2019-06-01 01:30:49 +0200

    c

--- js/main.js
+++ js/main.js
@@ -11,7 +11,9 @@ var onDocumentLoaded = function () {
         {filename: "1.jpg"},
         {filename: "2.jpg"},
         {filename: "3.jpg"},
-        {filename: "4.jpg"}
+        {filename: "4.jpg"},
+        {filename: "5.jpg"},
+        {filename: "6.jpg"}
     ];
 
     var display = function () {
